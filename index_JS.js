1bb315364c5e34fb9ca721715ee292ec55b5a9e2
BenWestholm <westhobm1621@uwec.edu>
2020-03-10 14:52:10 -0500

    These are mostly changes to the homepage.

---
+++ index_JS.js
@@ -0,0 +1,39 @@
+function selectVideoOption() {
+  document.getElementById("videoCircle").style.background = 'grey';
+}
+
+function unselectVideoOption() {
+  document.getElementById("videoCircle").style.background = 'rgba(0,0,0,.1)';
+}
+
+function selectAboutOption() {
+  document.getElementById("aboutCircle").style.background = 'grey';
+}
+
+function unselectAboutOption() {
+  document.getElementById("aboutCircle").style.background = 'rgba(0,0,0,.1)';
+}
+function selectPodcastOption() {
+  document.getElementById("podcastCircle").style.background = 'grey';
+}
+
+function unselectPodcastOption() {
+  document.getElementById("podcastCircle").style.background = 'rgba(0,0,0,.1)';
+}
+
+function selectArticlesOption() {
+  document.getElementById("articlesCircle").style.background = 'grey';
+}
+
+function unselectArticlesOption() {
+  document.getElementById("articlesCircle").style.background = 'rgba(0,0,0,.1)';
+}
+/*
+function selectedOption() {
+  document.getElementsByClassName("circle").style.background = 'grey';
+}
+
+function unselectOption() {
+  document.getElementsByClassName("circle").style.background = 'rgba(0,0,0,.1)';
+}*/
+
