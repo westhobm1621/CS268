fd07b386f8a1c9748e85e1da8866d07871eb1940
westhobm1621 <60725525+westhobm1621@users.noreply.github.com>
2020-04-13 03:55:09 -0500

    Delete about_page_js.js

--- about_page_js.js
+++
@@ -1,4 +0,0 @@
-function clickSubmit() {
-  document.getElementById("form").style.display = 'none';
-  document.getElementById("formResponse").innerHTML = "Interesting choice, thanks for sharing!";
-}
