5eb004629805200fda6c855fe0b6914f0b10e65b
BenWestholm <westhobm1621@uwec.edu>
2020-04-13 03:51:52 -0500

    about pages lowercase

---
+++ aboutpagejs.js
@@ -0,0 +1,4 @@
+function clickSubmit() {
+  document.getElementById("form").style.display = 'none';
+  document.getElementById("formResponse").innerHTML = "Interesting choice, thanks for sharing!";
+}
